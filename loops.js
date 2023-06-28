c2626591f5081cdc758e46a22bbd3af12b429e64
pvantessel <132936837+pvantessel@users.noreply.github.com>
2023-06-28 18:37:52 +0200

    les7-commit-1

--- loops.js
+++ loops.js
@@ -11,9 +11,11 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
+const star = '*';
 
-
-
+for (let i = 1; i < 6; i++) {
+    console.log(`${star.repeat(i)}`);
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
@@ -25,9 +27,13 @@
 // klaar!
 // ==========================================
 
-
-
-
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log(`loop...`);
+    } else {
+        console.log(`klaar!`);
+    }
+}
 
 // ==========================================
 // Opdracht 3: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -45,9 +51,15 @@
 // >> 9
 // ==========================================
 
-
-
-
+for (let i = 0; i < 10; i++) {
+    if (i < 3 ) {
+        console.log(i);
+    } else if (i >= 3 && i < 6) {
+        console.log(`> ${i}`);
+    } else {
+        console.log(`>> ${i}`);
+    }
+}
 
 // ==========================================
 // Opdracht 4 (BONUS): schrijf een for-loop die van 0 tot 100 loopt en de getallen print.
@@ -87,5 +99,13 @@
 // etc.
 // ==========================================
 
-
-
+for (let i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0 ) {
+        console.log(`FizzBuzz`);
+    } else if (i % 3 === 0) {
+        console.log(`Fizz`);
+    } else if (i % 5 === 0) {
+        console.log(`Buzz`);
+    } else
+        console.log(i);
+}
